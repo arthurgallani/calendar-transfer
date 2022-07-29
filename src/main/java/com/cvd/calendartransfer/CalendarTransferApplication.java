592e6e11ded887be3fdf67f04aaacb1a86ecc207
Arthur Gallani <arthurgallanialves@gmail.com>
2022-07-29 12:27:24 -0300

    init project

---
+++ src/main/java/com/cvd/calendartransfer/CalendarTransferApplication.java
@@ -0,0 +1,13 @@
+package com.cvd.calendartransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendarTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendarTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvd/calendartransfer/CalendarTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvd.calendartransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
